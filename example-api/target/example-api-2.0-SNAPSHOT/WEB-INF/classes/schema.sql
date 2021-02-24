1103d7bb2543fddc22adc638a7bb6516bdfa6593
ayush <jbytrain@gmail.com>
2021-02-24 23:44:51 +0530

    Removed target folders

--- example-api/target/example-api-2.0-SNAPSHOT/WEB-INF/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `example`
-(
-    `id`   int(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(50) DEFAULT NULL,
-    `description` varchar(200) DEFAULT NULL,
-    `createdOn` varchar(20) DEFAULT NULL,
-    `updatedOn` varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
